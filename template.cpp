fd2cc504cb2ef90ab9e6b134463a10599c3567f1
ermine27 <weviner@gmail.com>
2022-09-08 22:11:41 +0900

    update テンプレートファイル　空行削除だけ

--- template.cpp
+++ template.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 #define _overload2(a,b,f,...) f
 #define _overload3(a,b,c,f,...) f
-
 #define _rep1(i,n) for(int i = 0; i < (int)(n); ++i)
 #define _rep2(i,s,n) for(int i = (int)(s); i < (int)(n); ++i)
 #define rep(...) _overload3(__VA_ARGS__, _rep2, _rep1)(__VA_ARGS__)
@@ -15,7 +14,6 @@ using namespace std;
 #define _fore1(x,a) for(auto& x:a)
 #define _fore2(k,v,a) for(auto& [k,v] : a)
 #define fore(...) _overload3(__VA_ARGS__, _fore2, _fore1)(__VA_ARGS__)
-
 #define all(v) v.begin(), v.end()
 #define sz(x) ((int)(x).size())
 using ll = long long;
@@ -31,10 +29,8 @@ using mil = map<int,ll>;
 #define vls(x,...) vl x(__VA_ARGS__)
 #define vvis(x,h,...) vvi x(h,vi(__VA_ARGS__))
 #define vvls(x,h,...) vvl x(h,vl(__VA_ARGS__))
-
 template<typename T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
 template<typename T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
-
 template<typename... T> void in(T&... a){ (cin >> ... >> a); }
 template<typename T> void out(T& a){ cout << a << '\n'; }
 template<typename T, typename... T2> void out(T& a, T2&... b){ cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
