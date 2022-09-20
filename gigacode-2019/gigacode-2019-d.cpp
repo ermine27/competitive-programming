ad531ed2fa109a711a778ccb386a17f3599f695b
ermine27 <weviner@gmail.com>
2022-09-20 22:08:03 +0900

    reformat gigacode-2019-D

--- gigacode-2019/gigacode-2019-d.cpp
+++ gigacode-2019/gigacode-2019-d.cpp
@@ -24,32 +24,30 @@ template<typename... T> void in(T&... a){ (cin >> ... >> a); }
 template<typename T> void out(T& a){ cout << a << '\n'; }
 template<typename T, typename... T2> void out(T& a, T2&... b){ cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
 
-
-int main(){
-    int h,w;
-    ll k,v;
-    in(h,w,k,v);
-    vvls(a,h,w);
-    vvls(s,h+1,w+1);
-    rep(i,h) rep(j,w) in(a[i][j]);
+int main() {
+    int h, w;
+    ll k, v;
+    in(h, w, k, v);
+    vvls(a, h, w);
+    vvls(s, h + 1, w + 1);
+    rep(i, h) rep(j, w) in(a[i][j]);
 
     // 二次元累積和
-    repe(i,h) repe(j,w){
-        s[i][j] = s[i][j-1] + s[i-1][j] - s[i-1][j-1] + a[i-1][j-1];
+    repe(i, h) repe(j, w) {
+        s[i][j] = s[i][j - 1] + s[i - 1][j] - s[i - 1][j - 1] + a[i - 1][j - 1];
     }
 
     ll ans = 0;
-    rep(x1,h) rep(y1,w) repe(x2,x1,h) repe(y2,y1,w) {
-        ll area = (x2-x1) * (y2-y1);
-        if(area==0) continue;
+    rep(x1, h) rep(y1, w) repe(x2, x1, h) repe(y2, y1, w) {
+        ll area = (x2 - x1) * (y2 - y1);
+        if (area == 0) continue;
 
         ll cost = (s[x2][y2] - s[x1][y2] - s[x2][y1] + s[x1][y1]) + (area * k);
-        if(cost<=v){
+        if (cost <= v) {
             chmax(ans, area);
         } else {
             break;
         }
-
     }
 
     out(ans);
