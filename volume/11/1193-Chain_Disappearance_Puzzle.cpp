aa1ee0a5d28940f45d77d3e9c355a097e6ba1716
ermine27 <weviner@gmail.com>
2022-10-04 21:47:56 +0900

    refactor 1193-Chain_Disappearance_Puzzle

--- volume/11/1193-Chain_Disappearance_Puzzle.cpp
+++ volume/11/1193-Chain_Disappearance_Puzzle.cpp
@@ -19,7 +19,7 @@ using vi = vector<int>;
 using vl = vector<ll>;
 using vvi = vector<vi>;
 using vvl = vector<vl>;
-template<typename... T> void in(T&... a){ (cin >> ... >> a); }
+template<typename... T> int in(T&... a){ (cin >> ... >> a); return 1; }
 template<typename T> void out(T& a){ cout << a << '\n'; }
 template<typename T, typename... T2> void out(T& a, T2&... b){ cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
 
@@ -29,61 +29,35 @@ int main() {
     int h;
     vvi s;
 
-    while (true) {
-        in(h);
-        if (h == 0) break;
-
+    while (in(h) && h != 0) {
         int score = 0;
         s.assign(h + 1, vi(5, -1));
         repe(i, h) rep(j, 5) in(s[i][j]);
 
-        while (true) {
-            bool check = false;
+        bool check = true;
+        while (check) {
+            check = false;
             repe(i, h) {
-                if (s[i][0] != EMPTY && s[i][0] == s[i][1] && s[i][1] == s[i][2] && s[i][2] == s[i][3] && s[i][3] == s[i][4]) {
-                    score += 5 * s[i][0];
-                    rep(j, 5) s[i][j] = EMPTY;
-                    check = true;
-                    break;
-                }
-
-                rep(j, 2) {
-                    if (s[i][j] != EMPTY && s[i][j] == s[i][j + 1] && s[i][j + 1] == s[i][j + 2] && s[i][j + 2] == s[i][j + 3]) {
-                        score += 4 * s[i][j];
-                        rep(k, j, j + 4) s[i][k] = EMPTY;
-                        check = true;
-                        break;
-                    }
-                }
-
                 rep(j, 3) {
                     if (s[i][j] != EMPTY && s[i][j] == s[i][j + 1] && s[i][j + 1] == s[i][j + 2]) {
-                        score += 3 * s[i][j];
-                        rep(k, j, j + 3) s[i][k] = EMPTY;
+                        int con = 3;
+                        for (int k = j + 3; s[i][j] == s[i][k] && k < 5; ++k)
+                            ++con;
+                        score += con * s[i][j];
+
+                        rep(k, j, j + con) s[i][k] = EMPTY;
                         check = true;
                         break;
                     }
                 }
             }
 
-            if (check) {
-                rep(j, 5) {
-                    int i = h;
-                    while (i >= 1) {
-                        if (s[i][j] == EMPTY) {
-                            bool rest = false;
-                            for (int k = i; k > 0; --k) {
-                                s[k][j] = s[k - 1][j];
-                                if (s[k][j] != EMPTY) rest = true;
-                            }
-                            if (rest == false) break;
-                        } else {
-                            --i;
-                        }
+            rep(j, 5) {
+                for (int i = h; i > 1; --i) {
+                    for (int k = h; k > 0; --k) {
+                        if (s[k][j] == EMPTY) swap(s[k][j], s[k - 1][j]);
                     }
                 }
-            } else {
-                break;
             }
         }
 
