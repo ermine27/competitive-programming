f270c3d9b41657f74ad4166a339964c8685a11f7
ermine27 <weviner@gmail.com>
2022-11-14 22:00:39 +0900

    solved ITP2_9 A,B,C,D

---
+++ src/aoj/course/ITP2/ITP2_9_B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) cin >> b[i];
+    vector<int> c;
+    // set_union 2つのソート済集合の積集合を得る
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(), inserter(c, c.end()));
+
+    for (int i = 0; i < (int)c.size(); ++i) {
+        cout << c[i] << endl;
+    }
+
+    return 0;
+}
