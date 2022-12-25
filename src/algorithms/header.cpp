c09152c918175215e06aa7ad17a347b143ae6c0f
ermine27 <weviner@gmail.com>
2022-12-25 19:57:26 +0900

    add テンプレートにyesno, yes, noマクロを追加した

--- src/algorithms/header.cpp
+++ src/algorithms/header.cpp
@@ -38,6 +38,9 @@ template<typename T> void invec2(T &x){ rep(i, sz(x)) invec(x[i]); }
 template<typename T> void out(T& a){ cout << a << '\n'; }
 template<typename T, typename... T2> void out(T& a, T2&... b){ cout << a; (cout << ... << (cout << ' ', b)); cout << '\n'; }
 template<typename T> void outvec(const vector<T>& v, char d = '\n'){ rep(i,sz(v)) cout << v[i] << (i == sz(v)-1 ? '\n' : d);}
+#define yesno(x) cout << ((x) ? "Yes" : "No") << endl;
+#define yes(x) if(x){ cout << "Yes" << endl; exit(0); }
+#define no(x) if(x){ cout << "No" << endl; exit(0); }
 const int INF = 1.1e9;
 const ll INFLL = 4.4e18;
 const int dx[] = {1, 0, -1, 0, 1, -1, -1, 1};
