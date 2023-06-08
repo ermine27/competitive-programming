0bb9cca7c95c0a6e0eea5ae604e7960cc654ffe9
ermine27 <weviner@gmail.com>
2023-06-09 00:01:05 +0900

    update tessoku-book A09 二次元いもす法を使った正しい解き方に書き換え

--- src/atcoder/tessoku-book/A09.cpp
+++ src/atcoder/tessoku-book/A09.cpp
@@ -1,30 +1,43 @@
+/*
+二次元いもす法＋二次元累積和
+
+参考：二次元いもす法
+https://imoz.jp/algorithms/imos_method.html
+https://zenn.dev/reputeless/articles/article-2d-imos
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define repe(i, n) for (int i = 1; i <= (int)(n); ++i)
 using ll = long long;
 using vi = vector<int>;
 using vl = vector<ll>;
 
 int main() {
-    int h, w, n, ans = 0;
+    int h, w, n;
     cin >> h >> w >> n;
     vector<vi> imos(h + 1, vi(w + 1));
     rep(i, n) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        for (int j = a; j <= c; j++) {
-            imos[j - 1][b - 1]++;
-            imos[j - 1][d]--;
-        }
+        a--;
+        b--;
+        imos[a][b]++;
+        imos[a][d]--;
+        imos[c][b]--;
+        imos[c][d]++;
     }
+
+    // 二次元累積和
     rep(i, h) {
-        ans = 0;
-        rep(j, w) {
-            ans += imos[i][j];
-            cout << ans << ' ';
-        }
+        rep(j, w) { imos[i][j + 1] += imos[i][j]; }
+    }
+    rep(j, w) {
+        rep(i, h) { imos[i + 1][j] += imos[i][j]; }
+    }
+
+    rep(i, h) {
+        rep(j, w) { cout << imos[i][j] << ' '; }
         cout << endl;
     }
     return 0;
