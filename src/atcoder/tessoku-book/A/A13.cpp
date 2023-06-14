be2cbd82272ed4e58fe11b86451ae066dab4456d
ermine27 <weviner@gmail.com>
2023-06-14 22:54:23 +0900

    update tessoku-book A13 別解：尺取り法を使って解く

--- src/atcoder/tessoku-book/A/A13.cpp
+++ src/atcoder/tessoku-book/A/A13.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define repe(i, n) for (int i = 1; i <= (int)(n); ++i)
 using ll = long long;
 using vi = vector<int>;
 using vl = vector<ll>;
 
+/*
+最初に考えた解法
+左の要素から順に、Ai + k の位置を二分探索で探す。
+*/
+#if 0
 int main() {
     int n, k;
     ll ans = 0;
@@ -18,6 +22,28 @@ int main() {
         auto f = a.begin() + i;
         auto t = upper_bound(f, a.end(), a[i] + k);
         ans += t - f - 1;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
+#endif
+
+/*
+公式の解法
+尺取り法を使って解く
+*/
+int main() {
+    int n, k, rpos = 0;
+    ll ans = 0;
+    cin >> n >> k;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+
+    rep(lpos,n){
+        while(rpos < n && a[rpos] - a[lpos] <= k)
+            ++rpos;
+        ans += rpos - lpos - 1;
     }
 
     cout << ans << endl;
